b9bb5ae42b9a1d9b461ee1cd38f22785f7dc75f2
SHEN Jiamin <shen_jiamin@outlook.com>
2019-10-29 17:44:12 +0800

    modularize client

---
+++ client/utils.cxx
@@ -0,0 +1,5 @@
+//
+// Created by ncl on 29/10/19.
+//
+
+#include "utils.hxx"
---
+++ client/utils.hxx
@@ -0,0 +1,8 @@
+//
+// Created by ncl on 29/10/19.
+//
+
+#ifndef ENCLAVERAFT_UTILS_HXX
+#define ENCLAVERAFT_UTILS_HXX
+
+#endif //ENCLAVERAFT_UTILS_HXX
