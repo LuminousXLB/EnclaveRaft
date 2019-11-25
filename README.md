3dd7035f129a57929ca6ef9c3326c636a9cd8796
SHEN Jiamin <shen_jiamin@outlook.com>
2019-11-25 08:40:16 +0800

    add document

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # EnclaveRaft
 Implement Raft in an Enclave
+
+## How to run
+
+### Prerequisites
+
+- SGX-SDK
+<https://github.com/intel/linux-sgx/>
+- SGX-SSL
+<https://github.com/intel/intel-sgx-ssl>
+
+If the sgx-sdk is not installed in  `/opt/intel/sgxsdk`, specify the path using 
+```cmake
+set(SGX_DIR <sgxsdk-path>)
+```
+
+If the sgx-sdk is not installed in  `/opt/intel/sgxssl`, specify the path using 
+```cmake
+set(SGXSSL_SDK <sgxssl-path>)
+```
+
+### Build
+
+```console
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
+
+#### Server
+
+```console
+$ ./App_raft <server-id>
+```
+
+#### Client
+
+```console
+$ ./client <client-id> <request interval (ms)> <payload size (bytes)>
+```
+
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-gnome-terminal --tab -- bash -c "./cmake-build-debug/App_raft 1; date -Ins; read"
-gnome-terminal --tab -- bash -c "./cmake-build-debug/App_raft 2; date -Ins; read"
-gnome-terminal --tab -- bash -c "./cmake-build-debug/App_raft 3; date -Ins; read"
-gnome-terminal --tab -- bash -c "./cmake-build-debug/App_raft 4; date -Ins; read"
-gnome-terminal --tab -- bash -c "./cmake-build-debug/App_raft 5; date -Ins; read"
+gnome-terminal --tab -- bash -c "./App_raft 1; date -Ins; read"
+gnome-terminal --tab -- bash -c "./App_raft 2; date -Ins; read"
+gnome-terminal --tab -- bash -c "./App_raft 3; date -Ins; read"
+gnome-terminal --tab -- bash -c "./App_raft 4; date -Ins; read"
+gnome-terminal --tab -- bash -c "./App_raft 5; date -Ins; read"
 
